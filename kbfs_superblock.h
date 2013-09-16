fc3623e3dd680d2a322221ba10ae43216e9a38d1
William Brozas <wbrozas@gmail.com>
2013-09-15 21:10:01 -0400

    Add initial superblock struct (Subject to change)

---
+++ kbfs_superblock.h
@@ -0,0 +1,45 @@
+#ifndef KBFS_SUPERBLOCK_H
+#define KBFS_SUPERBLOCK_H
+#include <stdint.h>
+typedef struct _kbfs_superblock {
+    uint32_t s_inodes_count;
+    uint32_t s_blocks_count;
+    uint32_t s_r_blocks_count;
+    uint32_t s_free_blocks_count;
+    uint32_t s_free_inodes_count;
+    uint32_t s_first_data_block;
+    uint32_t s_log_block_size;
+    uint32_t s_log_frag_size;
+    uint32_t s_blocks_per_group;
+    uint32_t s_frags_per_group;
+    uint32_t s_inodes_per_group;
+    uint32_t s_mtime;
+    uint32_t s_wtime;
+    uint16_t s_mnt_count;
+    uint16_t s_max_mnt_count;
+    uint16_t s_magic;
+    uint16_t s_state;
+    uint16_t s_errors;
+    uint16_t s_minor_rev_level;
+    uint32_t s_lastcheck;
+    uint32_t s_checkinterval;
+    uint32_t s_creator_os;
+    uint32_t s_rev_level;
+    uint16_t s_def_resuid;
+    uint16_t s_def_resgid;
+    uint32_t s_first_ino;
+    uint16_t s_inode_size;
+    uint16_t s_block_group_nr;
+    uint32_t s_feature_compat;
+    uint32_t s_feature_incompat;
+    uint32_t s_feature_ro_compat;
+    uint8_t s_uuid[16];
+    char s_volume_name[16];
+    char s_last_mounted[64];
+    uint32_t s_algorithm_usage_bitmap;
+    uint8_t s_prealloc_blocks;
+    uint8_t s_prealloc_dir_blocks;
+    uint16_t s_padding1;
+    uint32_t s_reserved[204];
+} kbfs_superblock;
+#endif
